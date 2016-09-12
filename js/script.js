47ec40934faa82ebc61348e66e05b90e15c3d3c4
Céline Thiry <thiry.celi@gmail.com>
2016-09-12 19:56:57 +0200

    J'ai bien travaillé :D

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
-// Shorthand for $( document ).ready()
-$( document ).ready(function() {
+//Function to the css rule
+function isWideScreen() {
+	// retourne le résultat, qui peut être utilisé par le code qui appelle la fonction.
+	var width = parseFloat($(".main__intro-block").css("width"));
+	return width >= 690;
+}
 
-    
-    //Reload page when resize
-    $(window).on('resize',function(){location.reload();});
 
-    // Check dimension of query to adapt media query
-    $(document).ready(function() {
-        // run test on initial page load
-        checkSize();
-
-        // run test on resize of the window
-        $(window).resize(checkSize);
-    });
-
-    //Function to the css rule
-    function checkSize(){
-        if ($(".main__intro-block").css("width") >= "690px" ){
-        		console.log('je fais 690px !');
-            	//adapt margin to header height and body-container top and bottom margin
-                var marginLeftOfBodyContainer = parseFloat($('.body-container').css('margin-left'))+0;
-                $('.header').css('height',marginLeftOfBodyContainer);
-                $('.body-container').css('margin-top',marginLeftOfBodyContainer).css('margin-bottom',marginLeftOfBodyContainer);                
-        }
-    }
+function resizeContentForWideScreens() {
+	console.log('resizeContentForWideScreens');
+	//adapt margin to header height and body-container top and bottom margin
+    var marginLeftOfBodyContainer = parseFloat($('.body-container').css('margin-left'));
+    $('.header').css('height',marginLeftOfBodyContainer);
+    $('.body-container').css('margin-top',marginLeftOfBodyContainer).css('margin-bottom',marginLeftOfBodyContainer);  
 
     var figureHeight = $('figure').css('height');
-                $('.main__intro-block').css('height',figureHeight);
-                $('.button-block').css('height',figureHeight);
+    $('.main__intro-block').css('height',figureHeight);
+    $('.button-block').css('height',figureHeight); 
+}
 
+function resizeContentForSmallScreens() {
+	console.log('resizeContentForSmallScreens');
+	var headerHeight = $('.header').css('height');
+	$('.body-container').css('margin-top',headerHeight).css('margin-bottom',headerHeight); 	
+}
+
+function resizeContent() {
+	console.log("resize to content");
+    if (isWideScreen()) {
+   		resizeContentForWideScreens();
+    }
+    else {
+    	resizeContentForSmallScreens();
+    }
+}
+
+// Shorthand for $( document ).ready()
+$(document).ready(function(){
+    resizeContent();
 });
-    
+
+$(window).on('resize', function(){
+    resizeContent();
+});
